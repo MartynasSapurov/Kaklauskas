9f4fc29d0964fac783540df9c8390c702b795d1a
MartynasSapurov <127592293+MartynasSapurov@users.noreply.github.com>
2024-10-08 17:44:50 +0300

    Update Audio_csv_imort.py

--- Audio/Audio_csv_imort.py
+++ Audio/Audio_csv_imort.py
@@ -4,6 +4,12 @@ import datetime
 import json
 import paho.mqtt.client as mqtt
 import numpy as np
+import math
+
+def to_none(val):
+    if math.isnan(val):
+        return None
+    return val
 
 def np_encoder(object):
     if isinstance(object, np.generic):
@@ -12,7 +18,7 @@ def np_encoder(object):
 MQTT_HOST = "158.129.192.209"
 MQTT_PORT = 1883
 MQTT_KEEPALIVE_INTERVAL = 45
-USER = "Test"
+USER = "Natalija"
 
 def on_publish(client, userdata, mid):
     print("Message Published...")
@@ -26,47 +32,58 @@ def on_connect(client, userdata, flags, rc):
         client.subscribe(MQTT_TOPIC)
 
 
-client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "MR101") # client ID "mqtt-test"
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, "MR4") # client ID "mqtt-test"
 client.on_connect = on_connect
 client.on_publish = on_publish
 
 client.username_pw_set("vgtu", "dPDIhIs2k0Cb")
 
-df = pd.read_csv('Emotional Diamond.csv', sep=';', header=0, float_precision='legacy')
+df = pd.read_csv('MartPro3split2.csv', sep=',', header=0, float_precision='legacy')
 
-for column in df.columns:
-    for value in range(len(df)):
-        print(str(df[column][value]),str(column))
+"""
+print(datetime.datetime(2024, 9, 11, 19, 20, 00, 1, timezone.utc))
+print(datetime.datetime.now(timezone.utc))
+"""
 
-        try:
-            client.connect('158.129.192.209', 1883)
-            time = datetime.datetime.now(timezone.utc)
-            time_str = str(time+datetime.timedelta(-1, df['endPosSec'][value]))
-
-            transmit_data = {
-                "device": {
-                    "name": "MR101",
-                    "user": USER,
-                    "type": "data",
-                    "time": time_str,
-                },
-                "field": {
-                    "value": df[column][value],
-                    "state": "good",
-                    "friendly_name": str(column),
-                    "unit": ""
+try:
+    client.connect('158.129.192.209', 1883)
+    for column in df.columns:
+        for value in range(len(df)):
+            #print(str(df[column][value]),str(column))
+            try:
+                #client.connect('158.129.192.209', 1883)
+                #time = datetime.datetime.now(timezone.utc)
+                time = datetime.datetime(2024, 9, 11, 15, 31, 3, 240001, timezone.utc)
+                time_str = str(time+datetime.timedelta(0, df['endPosSec'][value]))
+                print(df['endPosSec'][value])
+                print(time_str)
+                transmit_data = {
+                    "device": {
+                        "name": "MR101",
+                        "user": USER,
+                        "type": "data",
+                        "time": time_str,
+                    },
+                    "field": {
+                        "value": df[column][value],
+                        "state": "good",
+                        "friendly_name": str(column),
+                        "unit": ""
+                    }
                 }
-            }
-            MQTT_TOPIC = "MR101/" + str(column) + "/"
-            MQTT_TOPIC = "MR101/"+str(column)+"/"
-            MQTT_MSG = json.dumps(transmit_data, default=np_encoder)
+                #MQTT_TOPIC = "MR101/" + str(column) + "/"
+                MQTT_TOPIC = "MR4/"+str(column)+"/"
+                MQTT_MSG = json.dumps(transmit_data, default=np_encoder)
 
-            client.publish(MQTT_TOPIC, MQTT_MSG)
+                client.publish(MQTT_TOPIC, MQTT_MSG)
 
-            client.loop_stop()
+                #client.loop_stop()
 
-        except:
-            print("Unable to write data")
+            except:
+                print("Unable to write data")
+    client.loop_stop()
+except:
+    print("Unable to connect")
 
 """
 print(df.columns)
